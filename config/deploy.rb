e79187718366d94d0bd6ff7c2bfc8aa06dea9660
Jack Bishop <jack.bishop@synctree.com>
2012-12-21 11:19:30 -0800

    adding basic capistrano setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,45 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require "capistrano/ext/multistage"
+
+# Application Name
+set :application, "omnisentry"
+
+# Where we are deploying to
+set :location, "ec2-23-20-227-105.compute-1.amazonaws.com"
+
+# Multistage deployment. Each one of these corresponds to a <env_name>.rb file in config/deploy/ which contains specific options for each stage
+set :stages, ["production"]
+set :default_stage, "production"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :deploy_via, :copy
+set :scm, :git
+set :repository,  "git@github.com:OmniSentry/omnisentry.git"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+# Setup connection options. Set up your ec2 permissions via ssh-add and this option will allow you to connect
+set :user, "deploy"
+set :use_sudo, false
+set :ssh_options, { :forward_agent => true }
+default_run_options[:pty] = true
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+# Set up the different roles. These can be different locations for more complex deployments, or the same for simple deployment.
+role :web, location
+role :app, location
+role :db, location, :primary => true
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+# Enable Capistrano use with rvm.
+require 'rvm/capistrano'
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+# Specify gemset you want to use on server.
+set :rvm_ruby_string, '1.9.3@omnisentry' 
+set :rvm_type, :user
+
+# Enable Capistrano use with Bundler
+require 'bundler/capistrano'
+
+# Run a bundle update on the remote (should not be necessary)
+namespace :bundle do
+  task :update do
+    run "cd #{current_path} && bundle update"
+  end
+end
