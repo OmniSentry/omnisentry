e89bb4ff770f65a4149b7e02af4380ad799e4b64
Jack Bishop <jack.bishop@synctree.com>
2012-12-21 10:56:02 -0800

    adding capistrano for deployment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omnisentry::Application.config.secret_token = 'd1b71c45b5d0119f9b716169a8d2a6fca7f993b541710b15c949983242bdc7fb85980a4fbee0f274288bd92560240eae61247a6378622a6a8517867293624a76'
